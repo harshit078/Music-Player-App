006e5144801f2abab29cd5f659f245916f5e7bc7
harshit078 <harshitsingh467@gmail.com>
2024-07-16 18:15:06 +0530

    Added Dropdown menu

---
+++ config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  name: "Harshit Singh",
+  username: "harshit078",
+  links: {
+    github: { name: "GitHub", url: "https://github.com/harshit078" },
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
